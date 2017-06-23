6714e86dfc0316cc1da7e02e604fe1ee8d9cee52
Koen Hufkens <koen.hufkens@gmail.com>
2017-06-23 11:46:06 -0400

    read in main data file and questionaire + merge (#2)
    * read in main data file and questionaire + merge
    
    * Update read_cropmonitor_data.r

---
+++ read_cropmonitor_data.r
@@ -0,0 +1,17 @@
+# Load main data file, including all indices
+df = readRDS("~/cropmonitor/cropmonitor.rds")
+
+# screen for test sites in the US / EU
+df = df[which(df$longitude > 70),]
+df = df[, -which(names(df) %in% "questionaireresults")]
+
+# read questionnaire data and merges with the original
+# data
+if (file.exists("~/cropmonitor/questionaire.xlsx")){
+  quest = readxl::read_excel("~/cropmonitor/questionaire.xlsx")
+  quest = quest[,-(2:9)]
+  df = merge(df, quest, by = 'reportid', all.x = TRUE)
+}
+
+# create unique field vector
+df$userfield = paste(df$uniqueuserid,df$uniquecropsiteid,sep = "-")
